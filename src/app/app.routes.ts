6325f40d1e556e825a9d10857f17729d59ff2165
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-21 16:41:35 -0300

    feat: rotas adicionadas (padrão, online e offline)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { OfflineComponent } from './components/offline/offline.component';
+import { OnlineComponent } from './components/online/online.component';
+import { HeaderComponent } from './components/header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'offline', component: OfflineComponent },
+    { path: 'online', component: OnlineComponent }
+];
