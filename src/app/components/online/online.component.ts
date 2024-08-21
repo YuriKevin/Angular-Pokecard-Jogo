aa36c0c1f341556032fecaf1a67f1ab4417353de
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-21 16:46:21 -0300

    feat: novo componente "online"

---
+++ src/app/components/online/online.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-online',
+  standalone: true,
+  imports: [],
+  templateUrl: './online.component.html',
+  styleUrl: './online.component.css'
+})
+export class OnlineComponent {
+
+}
