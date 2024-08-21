28b499393accf9d8a6a21859480f5ab9d64bf861
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-21 16:45:35 -0300

    feat: imports home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
