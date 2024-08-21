7781e141902b66836abfc438a2cd5828ad672f2b
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-21 16:46:04 -0300

    feat: novo componente "offline"

---
+++ src/app/components/offline/offline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offline',
+  standalone: true,
+  imports: [],
+  templateUrl: './offline.component.html',
+  styleUrl: './offline.component.css'
+})
+export class OfflineComponent {
+
+}
