b76a3868aaa926574441f932261ad11bdb11135f
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-21 16:44:43 -0300

    feat: efeito aparecer/desaparecer do header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  private lastScrollTop = 0;
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const navbar = this.el.nativeElement.querySelector('.navegacao');
+
+    if (currentScrollTop > this.lastScrollTop) {
+      // Rolando para baixo
+      this.renderer.addClass(navbar, 'hidden');
+    } else {
+      // Rolando para cima
+      this.renderer.removeClass(navbar, 'hidden');
+    }
+
+    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+  }
 }
