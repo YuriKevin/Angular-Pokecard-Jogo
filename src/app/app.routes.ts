6e38a722351901eb29020d64171ca549c195d147
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-23 19:41:15 -0300

    feat: nova rota modo campanha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { OfflineComponent } from './components/offline/offline.component';
 import { OnlineComponent } from './components/online/online.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DueloComponent } from './components/duelo/duelo.component';
+import { CampanhaComponent } from './components/campanha/campanha.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'offline', component: OfflineComponent },
     { path: 'online', component: OnlineComponent },
-    { path: 'duelo', component: DueloComponent }
-];
+    { path: 'duelo', component: DueloComponent },
+    { path: 'campanha', component: CampanhaComponent }
+]
