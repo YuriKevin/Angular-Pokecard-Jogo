9cd436a9e78282b68bb006408935c65816cec2b0
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-22 21:19:46 -0300

    feat: variáveis de controle do DOM

--- src/app/components/duelo/duelo.component.ts
+++ src/app/components/duelo/duelo.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-duelo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './duelo.component.html',
   styleUrl: './duelo.component.css'
 })
 export class DueloComponent {
+  inicioDiv:boolean = false;
+  jogadaDiv:boolean = false;
+  mostrarCartas:boolean = false;
+  terminoPartida:boolean = false;
+  venceu:boolean = false;
+  perdeu:boolean = false;
+  abrirCarta:boolean = true;
 
 }
