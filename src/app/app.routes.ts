e9024d1d1aac2bd6602f1e1cb6ba834d623afc60
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-22 21:19:05 -0300

    feat: nova rota de duelo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './components/home/home.component';
 import { OfflineComponent } from './components/offline/offline.component';
 import { OnlineComponent } from './components/online/online.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DueloComponent } from './components/duelo/duelo.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'offline', component: OfflineComponent },
-    { path: 'online', component: OnlineComponent }
+    { path: 'online', component: OnlineComponent },
+    { path: 'duelo', component: DueloComponent }
 ];
