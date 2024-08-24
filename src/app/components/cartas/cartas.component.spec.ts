3e3a058738cd4a55c36a7c795ac08954dbaab568
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-24 14:26:00 -0300

    feat: cartas component gerado

---
+++ src/app/components/cartas/cartas.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CartasComponent } from './cartas.component';
+
+describe('CartasComponent', () => {
+  let component: CartasComponent;
+  let fixture: ComponentFixture<CartasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CartasComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CartasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/cartas/cartas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartas',
+  standalone: true,
+  imports: [],
+  templateUrl: './cartas.component.html',
+  styleUrl: './cartas.component.css'
+})
+export class CartasComponent {
+
+}
