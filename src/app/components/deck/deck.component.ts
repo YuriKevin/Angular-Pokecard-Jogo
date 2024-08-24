c97297ee6af74a83b0146a2d96ba4ec940997e59
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-24 14:26:14 -0300

    feat: deck component gerado

---
+++ src/app/components/deck/deck.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deck',
+  standalone: true,
+  imports: [],
+  templateUrl: './deck.component.html',
+  styleUrl: './deck.component.css'
+})
+export class DeckComponent {
+
+}
