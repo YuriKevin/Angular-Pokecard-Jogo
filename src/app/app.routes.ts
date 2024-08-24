298f69439a18b846288c42ceb271f771c12af951
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-24 14:36:50 -0300

    delete: rotas removidas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { OfflineComponent } from './components/offline/offline.component';
-import { OnlineComponent } from './components/online/online.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DueloComponent } from './components/duelo/duelo.component';
 import { CampanhaComponent } from './components/campanha/campanha.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'offline', component: OfflineComponent },
-    { path: 'online', component: OnlineComponent },
     { path: 'duelo', component: DueloComponent },
-    { path: 'campanha', component: CampanhaComponent }
+    { path: 'campanha', component: CampanhaComponent },
 ]
