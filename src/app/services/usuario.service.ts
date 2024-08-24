2392a2e3b463290e91857b93068ebbf77657564c
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-24 14:38:22 -0300

    feat: usuarioService

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from '../model/usuario';
+import { Card } from '../model/card';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  usuario:Usuario = {
+    nome: 'Yuri',
+    cards: [],
+    deck: [],
+    imagem: 'ash.png',            
+    batalhaAtual: 1
+  };
+  
+  constructor() {
+    /*const usuarioString = localStorage.getItem('usuario');
+    if (usuarioString) {
+      this.usuario = JSON.parse(usuarioString) as Usuario;
+    }*/
+   }
+
+  getUsuario():Usuario{
+    return this.usuario;
+  }
+  setDeck(deck:Card[]){
+    this.usuario.deck = deck;
+  }
+  addNovaCarta(card:Card){
+    if (!this.getUsuario().cards.some(carta => carta.id === card.id)) {
+      this.usuario?.cards.push(card);
+    }
+  }
+}
