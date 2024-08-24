703ff8b7ddd7010c49f0a38792e358ad18a16afa
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-24 14:26:44 -0300

    feat: routerlink adicionado

--- src/app/components/campanha/campanha.component.ts
+++ src/app/components/campanha/campanha.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-campanha',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, RouterModule],
   templateUrl: './campanha.component.html',
   styleUrl: './campanha.component.css'
 })
