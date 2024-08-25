922a73790582d8cece99bf6ad5dd409633c60748
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-25 00:18:45 -0300

    feat: nova rota adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DueloComponent } from './components/duelo/duelo.component';
 import { CampanhaComponent } from './components/campanha/campanha.component';
+import { DeckComponent } from './components/deck/deck.component';
+import { CartasComponent } from './components/cartas/cartas.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'duelo', component: DueloComponent },
     { path: 'campanha', component: CampanhaComponent },
+    { path: 'deck', component: DeckComponent },
+    { path: 'cartas', component: CartasComponent },
 ]
