8cab8fe6edb7f7a20000b5e20ca569fb1b4a9ac5
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-25 00:25:58 -0300

    feat: dados são carregados no construtor a partir das variáveis de serviço

--- src/app/components/deck/deck.component.ts
+++ src/app/components/deck/deck.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Card } from '../../model/card';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-deck',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, HeaderComponent],
   templateUrl: './deck.component.html',
   styleUrl: './deck.component.css'
 })
-export class DeckComponent {
+export class DeckComponent implements OnInit{
+  deck!:Card[];
+  cartas!:Card[];
+  caminhoImagem:string = 'assets/images/cards/';
+
+  constructor(private usuarioService:UsuarioService){}
+
+  
+  ngOnInit(): void {
+      this.deck = this.usuarioService.usuario.deck;
+      this.cartas = this.usuarioService.usuario.cards;
+  }
 
 }
