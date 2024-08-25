38312877bc74ecb9d48283d2ece7e54dd26523a7
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-25 00:23:47 -0300

    feat: método bgOnInit agora carrega os dados da próxima batalha

--- src/app/components/campanha/campanha.component.ts
+++ src/app/components/campanha/campanha.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { RouterModule } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
+import { BatalhaService } from '../../services/batalha.service';
+import { Treinador } from '../../model/treinador';
 
 @Component({
   selector: 'app-campanha',
@@ -10,6 +13,18 @@ import { RouterModule } from '@angular/router';
   templateUrl: './campanha.component.html',
   styleUrl: './campanha.component.css'
 })
-export class CampanhaComponent {
+export class CampanhaComponent implements OnInit{
   dialogoDiv:boolean = false;
+  proximoOponente!:Treinador;
+  caminhoImagemOponente:string = '';
+
+  constructor(private usuarioService:UsuarioService, private batalhaService:BatalhaService){}
+
+  ngOnInit(): void {
+    const numeroBatalhaAtual = this.usuarioService.usuario.batalhaAtual; 
+      this.proximoOponente = this.batalhaService.batalhar(numeroBatalhaAtual);
+      const caminhoBaseImagem = 'assets/images/personagens/';
+      this.caminhoImagemOponente = caminhoBaseImagem+this.proximoOponente.imagem;
+  }
+
 }
