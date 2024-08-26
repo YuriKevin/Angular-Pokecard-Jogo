d853a0ad6a4a4a7e4f924c677bdbb9b023f0e033
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-26 12:27:17 -0300

    feat: batalhas dos líderes de ginásio adicionadas!

--- src/app/services/batalha.service.ts
+++ src/app/services/batalha.service.ts
@@ -14,11 +14,61 @@ export class BatalhaService {
   constructor(private cardService:CardService) { }
 
   batalhar(numeroDaBatalha:number):Treinador{
-    if(numeroDaBatalha==1){
-      return this.batalha1();
-    }
-    else{
-      return this.batalha1();
+    switch(numeroDaBatalha){
+      case 1:
+      return this.batalha6();
+      break;
+
+      case 2:
+      return this.batalha2();
+      break;
+
+      case 3:
+      return this.batalha3();
+      break;
+
+      case 4:
+      return this.batalha4();
+      break;
+
+      case 5:
+      return this.batalha5();
+      break;
+
+      case 6:
+      return this.batalha6();
+      break;
+
+      case 7:
+      return this.batalha7();
+      break;
+
+      case 8:
+      return this.batalha8();
+      break;
+
+      case 9:
+      return this.batalha9();
+      break;
+
+      case 10:
+      return this.batalha10();
+      break;
+
+      case 11:
+      return this.batalha11();
+      break;
+
+      case 12:
+      return this.batalha12();
+      break;
+
+      case 13:
+      return this.batalha13();
+      break;
+
+      default:
+        return this.batalha1();
     }
   }
 
@@ -33,6 +83,33 @@ export class BatalhaService {
   }
   batalha4(){
     return this.treinadorAtual = this.gerarTreinadorProfessor();
+  }
+  batalha5(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Brock', 'Lutador', 'brock.png', 'Este é o líder do ginásio de lutadores.');
+  }
+  batalha6(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Misty', 'Água', 'misty.png', 'Esta é a líder do ginásio de Água.');
+  }
+  batalha7(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Blaine', 'Fogo', 'blaine.png', 'Este é o líder do ginásio de fogo.');
+  }
+  batalha8(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Erica', 'Grama', 'erica.png', 'Esta é a líder do ginásio de grama.');
+  }
+  batalha9(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Falkner', 'Voador', 'jasmine.png', 'Este é o líder do ginásio de voadores.');
+  }
+  batalha10(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Sabrina', 'Psiquico', 'sabrina.png', 'Esta é a líder do ginásio psíquico.');
+  }
+  batalha11(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Lt. Surge', 'Elétrico', 'surge.png', 'Esta é o líder do ginásio Elétrico.');
+  }
+  batalha12(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Jasmine', 'Metal', 'jasmine.png', 'Esta é a líder do ginásio de metal.');
+  }
+  batalha13(){
+    return this.treinadorAtual = this.gerarLiderDeGinasio('Agatha', 'Sombrio', 'agatha.png', 'Esta é a líder do ginásio sombrio.');
   }
 
   gerarTreinadorShadow(dificuldade:String):Treinador{
@@ -69,7 +146,7 @@ export class BatalhaService {
       dica: 'O professor não possui cartas muito fortes, seu foco está no laboratório.'
     };
   }
-  gerarLiderDeGinasioMisty(nome:string, elemento:string, imagem:string, dica:string){
+  gerarLiderDeGinasio(nome:string, elemento:string, imagem:string, dica:string){
     return this.treinadorAtual = {
       nome: nome,
       dificuldade: 'Médio',
