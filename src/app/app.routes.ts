a3ad82a71127bf6ab1a052e0125ca41d58cce274
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-26 16:51:02 -0300

    feat: novas rotas de novos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { CampanhaComponent } from './components/campanha/campanha.component';
 import { DeckComponent } from './components/deck/deck.component';
 import { CartasComponent } from './components/cartas/cartas.component';
 import { GuiaComponent } from './components/guia/guia.component';
+import { SkinsComponent } from './components/skins/skins.component';
+import { JogarNovamenteComponent } from './components/jogar-novamente/jogar-novamente.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -14,4 +16,6 @@ export const routes: Routes = [
     { path: 'deck', component: DeckComponent },
     { path: 'cartas', component: CartasComponent },
     { path: 'guia', component: GuiaComponent },
+    { path: 'skins', component: SkinsComponent },
+    { path: 'jogar-novamente', component: JogarNovamenteComponent },
 ]
