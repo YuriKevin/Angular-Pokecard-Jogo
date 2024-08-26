3ed6db5e29726f4d4021094e5d69a4b9a56b1a7b
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-26 15:42:06 -0300

    fix: manipulando atributos

--- src/app/components/campanha/campanha.component.ts
+++ src/app/components/campanha/campanha.component.ts
@@ -5,35 +5,38 @@ import { RouterModule } from '@angular/router';
 import { UsuarioService } from '../../services/usuario.service';
 import { BatalhaService } from '../../services/batalha.service';
 import { Treinador } from '../../model/treinador';
+import { DialogoComponent } from '../dialogo/dialogo.component';
+import { DialogoService } from '../../services/dialogo.service';
 
 @Component({
   selector: 'app-campanha',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, RouterModule],
+  imports: [CommonModule, HeaderComponent, RouterModule, DialogoComponent],
   templateUrl: './campanha.component.html',
   styleUrl: './campanha.component.css'
 })
 export class CampanhaComponent implements OnInit{
-  dialogoDiv:boolean = false;
   proximoOponente!:Treinador;
   caminhoImagemOponente:string = '';
   mensagem:string='';
   jogarBotao!:boolean;
   incompletoBotao!:boolean;
+  numeroBatalha!:number;
 
   constructor(private usuarioService:UsuarioService, private batalhaService:BatalhaService){}
 
   ngOnInit(): void {
     const numeroBatalhaAtual = this.usuarioService.usuario.batalhaAtual; 
-      this.proximoOponente = this.batalhaService.batalhar(numeroBatalhaAtual);
-      const caminhoBaseImagem = 'assets/images/personagens/';
-      this.caminhoImagemOponente = caminhoBaseImagem+this.proximoOponente.imagem;
-      if(this.usuarioService.getTamanhoDeck()>19){
-        this.jogarBotao = true;
-      }
-      else{
-        this.incompletoBotao = true;
-      }
+    this.numeroBatalha = numeroBatalhaAtual;
+    this.proximoOponente = this.batalhaService.batalhar(numeroBatalhaAtual);
+    const caminhoBaseImagem = 'assets/images/personagens/';
+    this.caminhoImagemOponente = caminhoBaseImagem+this.proximoOponente.imagem;
+    if(this.usuarioService.getTamanhoDeck()>19){
+      this.jogarBotao = true;
+    }
+    else{
+      this.incompletoBotao = true;
+    }
   }
 
 }
