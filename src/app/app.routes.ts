02dcb6db4c59fb7a690a362970ded5cb361c35b8
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-25 23:18:35 -0300

    feat: rota "guia" adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { DueloComponent } from './components/duelo/duelo.component';
 import { CampanhaComponent } from './components/campanha/campanha.component';
 import { DeckComponent } from './components/deck/deck.component';
 import { CartasComponent } from './components/cartas/cartas.component';
+import { GuiaComponent } from './components/guia/guia.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -12,4 +13,5 @@ export const routes: Routes = [
     { path: 'campanha', component: CampanhaComponent },
     { path: 'deck', component: DeckComponent },
     { path: 'cartas', component: CartasComponent },
+    { path: 'guia', component: GuiaComponent },
 ]
