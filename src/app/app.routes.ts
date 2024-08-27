1abed21d4f7b1fefc44a11bebe3224bcedf549d7
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-27 10:06:05 -0300

    fix: ajustando rota de skins e jogar-novamente para treinadores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,7 @@ import { CampanhaComponent } from './components/campanha/campanha.component';
 import { DeckComponent } from './components/deck/deck.component';
 import { CartasComponent } from './components/cartas/cartas.component';
 import { GuiaComponent } from './components/guia/guia.component';
-import { SkinsComponent } from './components/skins/skins.component';
-import { JogarNovamenteComponent } from './components/jogar-novamente/jogar-novamente.component';
+import { TreinadoresComponent } from './components/treinadores/treinadores.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -16,6 +15,5 @@ export const routes: Routes = [
     { path: 'deck', component: DeckComponent },
     { path: 'cartas', component: CartasComponent },
     { path: 'guia', component: GuiaComponent },
-    { path: 'skins', component: SkinsComponent },
-    { path: 'jogar-novamente', component: JogarNovamenteComponent },
+    { path: 'treinadores', component: TreinadoresComponent }
 ]
