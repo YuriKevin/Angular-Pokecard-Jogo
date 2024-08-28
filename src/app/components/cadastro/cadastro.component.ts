a4f122fa54da9f90751459c495b5bbd341f1d32e
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-28 18:18:24 -0300

    feat: método cadastrar + imports

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Usuario } from '../../model/usuario';
+import { Router } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, HeaderComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  nome:string=''
+  usuario!:Usuario;
 
+  constructor(private router:Router, private usuarioService:UsuarioService){
+
+  }
+
+  cadastrar(){
+    localStorage.removeItem('usuario');
+      this.usuario = {
+        nome: this.nome,
+        cards: [],
+        deck: [],
+        imagem: 'shadow.png',            
+        batalhaAtual: 1,
+        campeaoLigaAmadora: false,
+        campeaoLigaMundial: false,
+        derrotouGiovanni:false
+      };
+      this.usuarioService.usuario = this.usuario;
+      localStorage.setItem('usuario', JSON.stringify(this.usuario));
+      this.router.navigate(['/campanha']);
+     }
 }
