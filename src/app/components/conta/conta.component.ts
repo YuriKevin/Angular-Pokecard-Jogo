15a562e2c92828f6767abdca800e4416fa329c6a
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-28 19:58:04 -0300

    feat: novo componente conta

---
+++ src/app/components/conta/conta.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conta',
+  standalone: true,
+  imports: [],
+  templateUrl: './conta.component.html',
+  styleUrl: './conta.component.css'
+})
+export class ContaComponent {
+
+}
