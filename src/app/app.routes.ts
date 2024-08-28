4d84f58ed0744ff584aa82dc67401dfd22df79e2
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-28 18:02:59 -0300

    feat: rotas adicionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,13 @@ import { CartasComponent } from './components/cartas/cartas.component';
 import { GuiaComponent } from './components/guia/guia.component';
 import { TreinadoresComponent } from './components/treinadores/treinadores.component';
 import { LigaAmadoraComponent } from './components/liga-amadora/liga-amadora.component';
+import { LigaMundialComponent } from './components/liga-mundial/liga-mundial.component';
+import { GiovanniComponent } from './components/giovanni/giovanni.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'cadastro', component: CadastroComponent },
     { path: 'duelo', component: DueloComponent },
     { path: 'campanha', component: CampanhaComponent },
     { path: 'deck', component: DeckComponent },
@@ -17,4 +21,6 @@ export const routes: Routes = [
     { path: 'guia', component: GuiaComponent },
     { path: 'treinadores', component: TreinadoresComponent },
     { path: 'liga-amadora', component: LigaAmadoraComponent },
+    { path: 'liga-mundial', component: LigaMundialComponent },
+    { path: 'final', component: GiovanniComponent },
 ]
