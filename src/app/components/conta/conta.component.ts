1f9aceb45d6ab649936bd637715c0992bcbefc56
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-28 20:08:44 -0300

    feat: atributo, construtor, métodos ngOnInit e alterar

--- src/app/components/conta/conta.component.ts
+++ src/app/components/conta/conta.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-conta',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './conta.component.html',
   styleUrl: './conta.component.css'
 })
-export class ContaComponent {
+export class ContaComponent implements OnInit{
+  nome:string = '';
+
+  constructor(private usuarioService:UsuarioService, private router:Router){
+
+  }
+
+  ngOnInit(): void {
+      this.nome = this.usuarioService.usuario.nome;
+  }
+
+  alterar(){
+    if(this.nome.length>0){
+      this.usuarioService.usuario.nome = this.nome;
+      localStorage.setItem('usuario', JSON.stringify(this.usuarioService.usuario));
+      this.router.navigate(['/campanha'])
+
+    }
+  }
 
 }
