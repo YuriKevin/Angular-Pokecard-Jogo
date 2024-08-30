2a73607b48fe2afbee8d09068a4d5744cc4546f2
Yuri Kevin <yurikevin1405@gmail.com>
2024-08-30 10:06:42 -0300

    feat: liberando todas as cartas do jogo nas cartas do usuário e salvando progresso

--- src/app/components/giovanni/giovanni.component.ts
+++ src/app/components/giovanni/giovanni.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { DialogoComponent } from '../dialogo/dialogo.component';
 import { Router, RouterModule } from '@angular/router';
 import { UsuarioService } from '../../services/usuario.service';
+import { CardService } from '../../services/card.service';
 
 @Component({
   selector: 'app-giovanni',
@@ -16,7 +17,7 @@ export class GiovanniComponent implements OnInit{
   dialogoDiv:boolean = false;
   voltarDiv:boolean = false;
 
-  constructor(private usuarioService:UsuarioService, private router:Router){}
+  constructor(private usuarioService:UsuarioService, private router:Router, private cardService:CardService){}
 
   ngOnInit(): void {
     if(this.usuarioService.usuario.batalhaAtual<32){
@@ -30,6 +31,8 @@ export class GiovanniComponent implements OnInit{
           setTimeout(()=>{
             this.voltarDiv = true;
             this.usuarioService.usuario.derrotouGiovanni = true;
+            this.usuarioService.usuario.cards = this.cardService.cards;
+            localStorage.setItem('usuario', JSON.stringify(this.usuarioService.usuario));
           }, 1000)
         }, 5000)
       }, 10000)
